b7176e2d77b7c54f5cd5c4cb50b99da9341d2593
Prayush Gupta <pratyush.gupta.in@gmail.com>
2026-07-17 10:46:10 +0530

    Time: 0 ms (100%), Space: 43.5 MB (89.19%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,16 +3,18 @@ class Solution {
         int r1 = m-1;
         int r2 = n-1;
         int w = m+n-1;
-        while(r2 >= 0){
-            if(r1 >=0 && nums1[r1] > nums2[r2]){
-                nums1[w] = nums1[r1];
+
+        while(r2>=0){
+            if(r1>=0 && nums1[r1]>nums2[r2]){
+                nums1[w]=nums1[r1];
                 r1--;
             }
             else{
-                nums1[w]= nums2[r2];
+                nums1[w]=nums2[r2];
                 r2--;
             }
             w--;
         }
+
     }
 }
